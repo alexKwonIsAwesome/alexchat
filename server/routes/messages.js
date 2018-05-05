451d2387bb57cde92338071c8c2d18e0ae5a219f
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-05-05 23:20:19 +0900

    Add POST messages router

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -6,8 +6,21 @@ var Message = require('./../models/Message');
 router.get('/', paginate, async function (req, res) {
   try {
     const { offset, limit } = req.query;
-    const messages = await Message.find().skip(offset).limit(limit);
-    res.send({ count: messages.length, messages});
+    const docs = await Message.find().sort({ date: 'desc' }).skip(offset).limit(limit);
+    res.send({ count: docs.length, messages: docs});
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post('/', async function (req, res) {
+  try {
+    const { username, message } = req.body;
+    const doc = await new Message({
+      username,
+      message,
+    }).save();
+    res.send(doc);
   } catch (e) {
     res.status(400).send(e);
   }
