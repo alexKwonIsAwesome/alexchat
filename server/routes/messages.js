146e69a789ee8fc69f811def2614c7f9d7e69bc3
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-05-05 21:59:57 +0900

    Add messages route

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var paginate = require('./../middlewares/paginate').paginate;
+var Message = require('./../models/Message');
 
-router.get('/', async function (req, res) {
+router.get('/', paginate, async function (req, res) {
   try {
-    res.send('asdf');
+    const { offset, limit } = req.query;
+    const messages = await Message.find().skip(offset).limit(limit);
+    res.send({ count: messages.length, messages});
   } catch (e) {
     res.status(400).send(e);
   }
